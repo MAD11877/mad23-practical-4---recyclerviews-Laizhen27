557fa1e05362c5396814fb01c1971f89765d5652
Wen Bin Koh <s10242132@connect.np.edu.sg>
2023-07-01 13:14:03 +0800

    complete practical 4 change recycler view number to 20

--- app/src/main/java/sg/edu/np/mad/practical41/RecyclerView.java
+++ app/src/main/java/sg/edu/np/mad/practical41/RecyclerView.java
@@ -18,7 +18,7 @@ public class RecyclerView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 20; i++) {
             Random random = new Random();
             int name = random.nextInt(Integer.MAX_VALUE - 10000000) + 10000000;
             int desc = random.nextInt(Integer.MAX_VALUE - 10000000) + 10000000;
